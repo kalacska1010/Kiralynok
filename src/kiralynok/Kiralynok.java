ca483744c7ba6d1ebf3e04d24805b39308db910f
KalacskaB <kalacska1010@gmail.com>
2021-02-18 10:44:40 +0100

    OOp 9. fealadatig

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -3,17 +3,43 @@ package kiralynok;
 import java.io.IOException;
 
 public class Kiralynok {
+    
+    private Tabla tabla;
+
+    public Kiralynok() {
+       tabla= new Tabla('#');
+       megoldasok();
+    }
+    
+    
+    
     public static void main(String[] args) throws IOException {
-       //4. feladat
+     Kiralynok kir = new Kiralynok();
+     kir.megoldasok();
+    }
+
+    private void megoldasok() {
+     feladat4();
+     feladat6();
+     feladat9();
+    }
+
+    private void feladat4() {
+        //4. feladat
         System.out.println("4. feladat: üres tábla megjelenítése");
-        Tabla tabla = new Tabla('#');
-        tabla.megjelenit();
-        
-       // 6. feladat
-       
-        System.out.println("6. feladat: feltöltött tábla:");        
+        tabla.megjelenit();}
+
+    private void feladat6() {
+        //6. feladat
+     System.out.println("6. feladat: feltöltött tábla:");        
         tabla.elhelyez(8);
-        tabla.megjelenit();
+        tabla.megjelenit(); }
+
+    private void feladat9() {
+    //9. feladat
+        System.out.println("9. feladat: üres sorok és oszlopok száma:");
+        System.out.println("sorok:"+tabla.uresSorokSzama());
+        System.out.println("oszlopok:"+tabla.uresOszlopokSzama());
     }
     
 }
